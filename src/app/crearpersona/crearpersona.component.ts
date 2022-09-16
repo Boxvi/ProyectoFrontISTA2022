858da6e9715b0756cfa70533853f7b055914bd5c
patri@Patukis <patricioperalta090@gmail.com>
2022-09-15 23:22:07 -0500

    Fin Sprint 1 c:

--- src/app/crearpersona/crearpersona.component.ts
+++ src/app/crearpersona/crearpersona.component.ts
@@ -11,5 +11,37 @@ export class CrearpersonaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayStyle = "none";
+  displayStyle2 = "none";
+  displayStyle3 = "none";
+  displayStyle4 = "none";
+  
+
+  
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  openPopup2() {
+    this.displayStyle2 = "block";
+  }
+  openPopup3() {
+    this.displayStyle3 = "block";
+  }
+  openPopup4() {
+    this.displayStyle4 = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
+  }
+  closePopup2() {
+    this.displayStyle2 = "none";
+  }
+  closePopup3() {
+    this.displayStyle3 = "none";
+  }
+  closePopup4() {
+    this.displayStyle4 = "none";
+  }
+  
 
 }
