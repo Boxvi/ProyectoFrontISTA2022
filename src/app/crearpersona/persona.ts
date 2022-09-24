cb2eefc5c7f0fa629e0eae5ef8fe126bcfcec3a1
patri@Patukis <patricioperalta090@gmail.com>
2022-09-23 20:13:11 -0500

    aaaa

---
+++ src/app/crearpersona/persona.ts
@@ -0,0 +1,9 @@
+export class Persona {
+    idpersona: number= 0;
+    cedula: string= "";
+    nombre: string="";
+    apellido: string="";
+    email: string="";
+    telefono: string="";
+    fecha_nacimiento: string="";
+}
